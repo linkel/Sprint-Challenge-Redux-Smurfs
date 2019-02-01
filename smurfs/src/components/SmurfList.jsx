9f4a93e671c083cebdd3efb45292645bc2e6ea4e
linkel <ironcarbide@gmail.com>
2019-02-01 09:27:20 -0800

    List item styling added.

--- smurfs/src/components/SmurfList.jsx
+++ smurfs/src/components/SmurfList.jsx
@@ -3,11 +3,11 @@ import redx from "../img/red-x.png";
 
 const SmurfList = props => {
     return (
-        <div className="list-wrapper">
-            {props.smurfs.map((smurf) => <li key={smurf.id} className="smurf">
+        <ul className="list-wrapper">
+            {props.smurfs.map((smurf) => <li><div key={smurf.id} className="smurf">
                 ID: {smurf.id} | {smurf.name} Smurf is {smurf.age} years old and {smurf.height >= 7 ? <span>an astonishing </span> : <span>a mere </span>}{smurf.height}cm tall.
-                <img alt="red_x" onClick={() => props.deleteSmurf(smurf.id)} className="red-x" src={redx}/></li>)}
-        </div>
+                </div><img alt="red_x" onClick={() => props.deleteSmurf(smurf.id)} className="red-x" src={redx}/></li>)}
+        </ul>
     )
 }
 
